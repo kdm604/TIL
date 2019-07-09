e5755b63cd29ab1b2d4238dc6b0d5f0a468e35bb
rnjseoals <kdm604@naver.com>
2019-07-09 17:34:29 +0900

    스타트캠프2일차

--- 00_Startcamp/02_Day/02_file_control.py
+++ 00_Startcamp/02_Day/02_file_control.py
@@ -2,6 +2,6 @@ import os
 
 os.chdir(r'C:\Users\student\TIL\00_Startcamp\02_Day\dummy')
 
-for fliename in os.listdir('.'):
+for filename in os.listdir('.'):
     # os.rename(fliename,f'SAMSUNG_{fliename}')
     os.rename(filename, filename.replace('SAMSUNG_','SSAFY_'))
