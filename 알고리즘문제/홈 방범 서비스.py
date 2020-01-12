ec56e148bd3a13a53abc4b16d988de00cdfca90d
권대민 <kdm604@naver.com>
2020-01-12 21:21:25 +0900

    홈방범서비스

---
+++ 알고리즘문제/홈 방범 서비스.py
@@ -0,0 +1,78 @@
+from collections import deque
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(x, y):
+    global ans, br
+    h = 0
+    if nxn[x][y] == 1:
+        h = 1
+    Q = deque()
+    Q.append((x, y))
+    visited[x][y] = 1
+
+    while len(Q):
+        x, y = Q.popleft()
+
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+
+            if visited[x][y] == K:
+                break
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0:
+                visited[nx][ny] = visited[x][y] + 1
+                if nxn[nx][ny] == 1:
+                    h += 1
+
+                Q.append((nx, ny))
+
+    if h * M >= K**2 + (K-1)**2:
+        if h > ans:
+            ans = h
+            br = 1
+
+
+T = int(input())
+
+for test in range(T):
+    N, M = map(int, input().split())
+    nxn = [[0 for _ in range(N)]for _ in range(N)]
+
+
+
+    for z in range(N):
+        nxn[z] = list(map(int, input().split()))
+    K = 0
+    cnt = 0
+    ans = 0
+    # 최대 비용을 찾기위한 써치
+    for x in range(N):
+        for y in range(N):
+            if nxn[x][y] == 1:
+                cnt += 1
+
+    price = cnt * M
+    # 최대비용으로 가능한 K값 구하기 이후에 써치하면서 K -1 씩하면서 정답 구하기
+    for k in range(1, 30):
+        kk = k**2 + (k-1)**2
+        if kk > price:
+            K = k - 1
+            break
+    if K > N+1:
+        K = N+1
+    RK = K
+    br = 0
+    for z in range(K):
+        if br == 1:
+            break
+        K = RK
+        K = K - z
+        for x in range(N):
+            for y in range(N):
+                visited = [[0 for _ in range(N)] for _ in range(N)]
+                bfs(x, y)
+
+    print("#%d %d" % (test+1, ans))
