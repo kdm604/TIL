063c11d14ae29f567d32c2bc1caf491f571106fd
권대민 <kdm604@naver.com>
2020-02-02 21:49:08 +0900

    유턴싫어

--- 알고리즘문제/유턴싫어.py
+++ 알고리즘문제/유턴싫어.py
@@ -1,29 +1,29 @@
 import sys
-sys.stdin = open("유턴싫어.txt")
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def dfs(x, y, dir):
-    for d in range(4):
-        if d == dir:
-            continue
-        nx = x + dx[d]
-        ny = y + dy[d]
-        if 0 <= nx < N and 0 <= ny < M:
-            if
-            dfs(nx, ny, d)
-
-
 
 N, M = map(int, input().split())
-nxm = [[0 for _ in range(M)]for _ in range(N)]
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+ans = 0
+br = 0
+nxm = [[0 for _ in range(M)]for _ in range(N)]
 for z in range(N):
     nxm[z] = list(input())
-
-
-for x in range(N):
-    for y in range(M):
-        if nxm[x][y] == '.':
-            dfs(x, y, 0)
+for i in range(N):
+    for j in range(M):
+        if nxm[i][j] == '.':
+            cnt = 0
+            for d in range(4):
+                ni = i + dx[d]
+                nj = j + dy[d]
+                if 0 <= ni < N and 0 <= nj < M:
+                    if nxm[ni][nj] == '.':
+                        cnt += 1
+            if cnt < 2:
+                ans = 1
+                br = 1
+                break
+    if br == 1:
+        break
+print(ans)
