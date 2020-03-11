b054668288904483d7e44d599f2e60057e8d089e
권대민 <kdm604@naver.com>
2020-03-11 11:51:32 +0900

    100만 이하의 모든소수

---
+++ 알고리즘문제/100만 이하의 모든소수.py
@@ -0,0 +1,13 @@
+n=1000000
+a = [False,False] + [True]*(n-1)
+primes = []
+
+for i in range(2, n+1):
+    if a[i]:
+        primes.append(i)
+    for j in range(2*i, n+1, i):
+        a[j] = False
+
+
+for i in range(len(primes)):
+    print(primes[i], end=" ")
